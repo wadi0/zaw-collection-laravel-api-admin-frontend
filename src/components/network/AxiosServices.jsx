af29b1a979ab8949cde397b003018df5dc03e93a
wadi <zabir4897@gmail.com>
2025-08-27 16:54:50 +0600

    render generate link add axios under

--- src/components/network/AxiosServices.jsx
+++ src/components/network/AxiosServices.jsx
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 // ✅ Create Axios instance
 const apiClient = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/',
+  baseURL: 'https://e-commerce-api-create.onrender.com/api/',
+      // 'http://127.0.0.1:8000/api/',
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json',
